68c09d4e7b3109a3f007fdc4d97145ba61809522
charlie_wutong <2459060612@qq.com>
2019-03-01 16:39:52 +0800

    初始化

---
+++ springioc/src/main/java/com/spring/ioc/config/MainConfiguration.java
@@ -0,0 +1,18 @@
+package com.spring.ioc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @AUTHOR: 小于
+ * @DATE: [2019/3/1  16:38]
+ * @DESC:
+ */
+@ComponentScan(basePackages = {"com.spring.ioc.basic"})
+@Configuration/*配置类*/
+public class MainConfiguration {
+
+
+
+
+}
